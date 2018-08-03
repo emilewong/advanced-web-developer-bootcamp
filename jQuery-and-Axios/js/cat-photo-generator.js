3cbdba5e95bd1072a3c73a811a079ed3b2e55643
hadhab haythem <hadhab@gmail.com>
2018-08-03 13:38:12 +0100

    Jquery - finished

---
+++ jQuery-and-Axios/js/cat-photo-generator.js
@@ -0,0 +1,9 @@
+$('#btn').click(function(){
+    $.getJSON("https://aws.random.cat/meow")
+    .done(function(data){
+        $('img').attr("src",data.file);
+    })
+    .fail(function(){
+        console.log("error");
+    });
+  });
