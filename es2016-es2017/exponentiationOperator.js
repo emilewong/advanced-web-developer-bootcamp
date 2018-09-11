bbca8ea410a8aabe454958dc575f888db6b0fe48
hadhab haythem <hadhab@gmail.com>
2018-09-11 13:44:00 +0100

    es2016-2017:es2016 exponentiation method

---
+++ es2016-es2017/exponentiationOperator.js
@@ -0,0 +1,11 @@
+//claculate the power of number
+var calculateNumber = 2**4;
+
+//example2
+
+var nums = [1,2,3,4];
+var total = 2;
+
+for(let i = 0; i < nums.length; i++){
+    total **= nums[i];
+}
