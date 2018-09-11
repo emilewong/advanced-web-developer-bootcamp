7a35dcd16e6921832ebf52c78c9ca23fe4a7d9b8
hadhab haythem <hadhab@gmail.com>
2018-09-11 14:29:14 +0100

    es2016-es2017: es2017 padStart & padEnd methods

---
+++ es2016-es2017/es2017/padStart-padEnd.js
@@ -0,0 +1,8 @@
+
+//padStart 
+
+"awesome".padStart(20/*max length of the output including ! at the beginnning*/,'!');
+//"!!!!!!!!!!!!!awesome"
+
+"awesome".padEnd(20,'!');
+//"awesome!!!!!!!!!!!!!"
